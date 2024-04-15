40e8c203df4f04b4ba5711bed027ccac666290f1
TranVanHuu732002 <Huukoi732002@gmail.com>
2024-04-15 23:25:10 +0700

    Switch form Login to form Register

--- src/containers/Public/Login.js
+++ src/containers/Public/Login.js
@@ -1,24 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, InputForm } from "../../components";
+import { useLocation } from "react-router-dom";
 
 function Login() {
+  const location = useLocation()
+  const [isRegister, setIsRegister] = useState(location.state?.flag);
+  useEffect(() => {
+    setIsRegister(location.state?.flag)
+    console.log(location.state?.flag);
+  },[location.state?.flag])
+
   return (
     <div className="bg-white w-[600px] mt-[12px] pt-[30px] pb-[40px] rounded-md shadow-sm justify-center">
-      <h3 className="font-semibold text-2xl justify-center flex">Đăng Nhập</h3>
+      <h3 className="font-semibold text-2xl justify-center flex">
+        {isRegister ? "Đăng Ký Tài Khoản" : "Đăng Nhập"}
+      </h3>
       <div className="w-full flex flex-col gap-5 p-[20px]">
-        <InputForm label={'SỐ ĐIỆN THOẠI' }/>
-        <InputForm label={'MẬT KHẨU ' }/>
-        <Button 
-        text='Đăng Nhập'
-        bgColor='bg-secondary1'
-        textColor='text-white'
-        fullWidth
+        {isRegister && <InputForm label={"HỌ TÊN"} />}
+        <InputForm label={"SỐ ĐIỆN THOẠI"} />
+        <InputForm label={"MẬT KHẨU "} />
+        <Button
+          text={isRegister ? "Đăng Ký" : "Đăng Nhập"}
+          bgColor="bg-secondary1"
+          textColor="text-white"
+          fullWidth
         />
 
         <div className="flex justify-between px-[12px] items-center">
-          <span className="text-[blue] hover:text-[red] cursor-pointer">Quên mật khẩu?</span>
-          <span className="text-[blue] hover:text-[red] cursor-pointer">Tạo tài khoản</span>
-
+          {isRegister ? (
+            <span>
+              Bạn đã có tài khoản?
+              <span
+                className="ml-[4px] text-[blue] hover:text-[red] cursor-pointer"
+                onClick={() => setIsRegister(false)}
+              >
+                Đăng nhập ngay
+              </span>
+            </span>
+          ) : (
+            <>
+              <span className="text-[blue] hover:text-[red] cursor-pointer">
+                Quên mật khẩu?
+              </span>
+              <span
+                className="text-[blue] hover:text-[red] cursor-pointer"
+                onClick={() => setIsRegister(true)}
+              >
+                Tạo tài khoản
+              </span>
+            </>
+          )}
         </div>
       </div>
     </div>

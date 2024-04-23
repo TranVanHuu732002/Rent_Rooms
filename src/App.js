150d82c09e657c776ce58b710d5adb89c68e3279
TranVanHuu732002 <Huukoi732002@gmail.com>
2024-04-23 20:41:13 +0700

    Finishes UI Navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import { Routes, Route } from "react-router-dom";
-import { Home, Login } from "./containers/Public";
+import {
+  Home,
+  Login,
+  RentalApartment,
+  RentalHouse,
+  RentalRoom,
+  RentalSpace,
+} from "./containers/Public";
 import { path } from "./utils/constant";
+import Homepage from "./containers/Public/Homepage";
 
 function App() {
   return (
     <div className="h-screen w-screen bg-primary">
       <Routes>
         <Route path={path.HOME} element={<Home />}>
+          <Route path={"*"} element={<Homepage />} />
           <Route path={path.LOGIN} element={<Login />} />
+          <Route path={path.CHO_THUE_CAN_HO} element={<RentalApartment />} />
+          <Route path={path.CHO_THUE_MAT_BANG} element={<RentalSpace />} />
+          <Route path={path.CHO_THUE_PHONG_TRO} element={<RentalRoom />} />
+          <Route path={path.NHA_CHO_THUE} element={<RentalHouse />} />
         </Route>
       </Routes>
     </div>
--- src/containers/Public/Navigation.js
+++ src/containers/Public/Navigation.js
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import { apiGetCategories } from "../../services/category";
+import { formatVietnameseToString } from "../../utils/constant";
+
+const notActive =
+  "hover:bg-secondary2 px-4 h-full flex items-center bg-secondary1";
+const active =
+  "hover:bg-secondary2 px-4 h-full flex items-center bg-secondary2";
 
 export const Navigation = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const response = await apiGetCategories();
+      if (response?.data.err === 0) {
+        setCategories(response.data.response);
+      }
+    };
+    fetchCategories();
+  }, []);
+
   return (
-    <div className="w-screen flex justify-center items-center text-white bg-secondary1 h-[40px] items-center"> 
-    <div className="w-1100">Navigation</div>
+    <div className="w-screen flex justify-center items-center text-white bg-secondary1 h-[40px] ">
+      <div className="w-1100 flex items-center h-full text-sm font-medium">
+        <NavLink
+          to={"/"}
+          className={({ isActive }) => (isActive ? active : notActive)}
+        >
+          Trang chủ
+        </NavLink>
+        {categories?.length > 0 &&
+          categories.map((item) => {
+            return (
+              <div
+                key={item.code}
+                className="h-full flex justify-center items-center"
+              >
+                <NavLink
+                  to={`${formatVietnameseToString(item.value)}`}
+                  className={({ isActive }) => (isActive ? active : notActive)}
+                >
+                  {item.value}
+                </NavLink>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };

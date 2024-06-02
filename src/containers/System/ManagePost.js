e5e2a1581fade819a2f5dc184209c4b80b311e40
TranVanHuu732002 <Huukoi732002@gmail.com>
2024-06-02 19:30:16 +0700

    Handle delete and filter follow status in page managePost

--- src/containers/System/ManagePost.js
+++ src/containers/System/ManagePost.js
@@ -3,15 +3,26 @@ import { useDispatch, useSelector } from "react-redux";
 import * as actions from "../../store/actions";
 import moment from "moment";
 import { Button, UpdatePost } from "../../components";
+import { apiDeletePost } from "../../services/post";
+import Swal from "sweetalert2";
 
 const ManagePost = () => {
   const dispatch = useDispatch();
   const { postOfCurrent, dataEdit } = useSelector((state) => state.post);
   const [isEdit, setIsEdit] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [status, setStatus] = useState('0');
+
+
+  const [updateData, setUpdateData] = useState(false);
 
   useEffect(() => {
     !dataEdit && dispatch(actions.getPostsLimitAdmin());
-  }, [dataEdit]);
+  }, [dataEdit, updateData]);
+
+  useEffect(() => {
+    setPosts(postOfCurrent);
+  }, [postOfCurrent]);
 
   useEffect(() => {
     !dataEdit && setIsEdit(false);
@@ -22,12 +33,58 @@ const ManagePost = () => {
       new Date().toDateString()
     );
 
+  const handleDeletePost = async (postId) => {
+    Swal.fire({
+      title: "Bạn có muốn xóa không?",
+      text: "Sau khi xóa, bạn sẽ không thể khôi phục bài đăng này!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Đồng ý",
+      cancelButtonText: "Hủy",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const response = await apiDeletePost(postId);
+        if (response?.data.err === 0) {
+          setUpdateData((prev) => !prev);
+          Swal.fire("Đã xóa!", "Xóa bài đăng thành công!", "info");
+        }
+      } else {
+        Swal.fire("Đã hủy", "Bài đăng của bạn vẫn còn!", "info");
+      }
+    });
+  };
+
+  const handleFilterByStatus = (statusCode) => {
+    setStatus(statusCode || '')
+    if (statusCode === 1) {
+      const activePost = postOfCurrent?.filter((item) =>
+        checkStatus(item?.overviews?.expired?.split(" ")[3])
+      );
+      setPosts(activePost);
+    } else if (statusCode === 2) {
+      const expiredPost = postOfCurrent?.filter(
+        (item) => !checkStatus(item?.overviews?.expired?.split(" ")[3])
+      );
+      setPosts(expiredPost);
+    } else{
+      setPosts(postOfCurrent)
+    }
+  };
+
   return (
     <div className="flex flex-col gap-6">
       <div className="py-4 border-b border-gray-200 flex items-center justify-between">
         <h1 className="text-3xl font-medium">Quản lí tin đăng</h1>
-        <select className="outline-none border p-2 border-gray-200 rounded-md">
-          <option value="">Lọc theo trạng thái</option>
+        <select
+          onChange={(e) => handleFilterByStatus(+e.target.value)}
+          value={status}
+          className="outline-none border p-2 border-gray-200 rounded-md"
+        >
+          <option value='0'>Lọc theo trạng thái</option>
+          <option value="1">Đang hoạt động</option>
+          <option value="2">Đã hết hạn</option>
         </select>
       </div>
 
@@ -61,12 +118,12 @@ const ManagePost = () => {
           </tr>
         </thead>
         <tbody>
-          {!postOfCurrent ? (
+          {!posts ? (
             <tr>
               <td>amskaskas</td>
             </tr>
           ) : (
-            postOfCurrent?.map((item) => {
+            posts?.map((item) => {
               return (
                 <tr key={item.id} className="hover:bg-gray-100">
                   <td className="border p-2 text-gray-600">
@@ -108,6 +165,7 @@ const ManagePost = () => {
                       bgColor="bg-red-400"
                       textColor="text-white"
                       text="Xóa"
+                      onClick={() => handleDeletePost(item.id)}
                     />
                   </td>
                 </tr>
